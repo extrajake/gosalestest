8540daa7f178e2ba58441e17f582c55f7ddd97fb
jake lacouvee <jlacouvee@live.ca>
2022-05-31 23:01:22 -0700

    update test api call

--- test.js
+++ test.js
@@ -1,10 +1,28 @@
-const content = document.querySelector("api-content");
-fetch("https://catfact.ninja/fact?max_length=140", {
-  mode: "cors"
-})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    console.log(response);
-  });
+var headers = {
+  "QB-Realm-Hostname": "gosales.quickbase.com",
+  "User-Agent": "FileService_Integration_V2.1",
+  Authorization: "b4zk43xsngt3xd7ximtbdbxycvc",
+  "Content-Type": "application/json"
+};
+
+var body = {
+  from: "bsazkzsm2",
+  select: [21],
+  where: "{33.CT.'6789'}",
+  sortBy: [{ fieldId: 3, order: "ASC" }],
+  groupBy: [{ fieldId: 33, grouping: "equal-values" }],
+  options: { skip: 0, top: 0, compareWithAppLocalTime: false }
+};
+
+const xmlHttp = new XMLHttpRequest();
+xmlHttp.open("POST", "https://api.quickbase.com/v1/records/query", true);
+for (const key in headers) {
+  xmlHttp.setRequestHeader(key, headers[key]);
+}
+xmlHttp.onreadystatechange = function() {
+  if (xmlHttp.readyState === XMLHttpRequest.DONE) {
+    console.log(xmlHttp.responseText);
+  }
+};
+
+xmlHttp.send(JSON.stringify(body));
