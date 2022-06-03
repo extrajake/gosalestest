43a4f83167fae5538fc07898ebd48b71adcfc9f1
jake lacouvee <jlacouvee@live.ca>
2022-06-03 13:08:59 -0700

    spelling corrections

--- test.js
+++ test.js
@@ -15,10 +15,13 @@ var body = {
 };
 
 const xmlHttp = new XMLHttpRequest();
+
 xmlHttp.open("POST", "https://api.quickbase.com/v1/records/query", true);
+
 for (const key in headers) {
   xmlHttp.setRequestHeader(key, headers[key]);
 }
+
 xmlHttp.onreadystatechange = function() {
   if (xmlHttp.readyState === XMLHttpRequest.DONE) {
     console.log(xmlHttp.responseText);
@@ -27,36 +30,36 @@ xmlHttp.onreadystatechange = function() {
 
 xmlHttp.send(JSON.stringify(body));
 
-xbody = {
-  data: [
-    {
-      "21": {
-        value:
-          "<tr><img  Alt=\"Manny Pinheiro67890\" ' src=&#039;https://gosales.quickbase.com/up/bsazkzsm2/a/r1/e22'/></tr></table>"
-      },
-      "33": {
-        value: "67890"
-      }
-    }
-  ],
-  fields: [
-    {
-      id: 21,
-      label: "Rep Card",
-      type: "rich-text"
-    },
-    {
-      id: 33,
-      label: "Agent ID",
-      type: "text"
-    }
-  ],
-  metadata: {
-    numFields: 2,
-    numRecords: 1,
-    skip: 0,
-    totalRecords: 1
-  }
-};
+// xbody = {
+//   data: [
+//     {
+//       "21": {
+//         value:
+//           "<tr><img  Alt=\"Manny Pinheiro67890\" ' src=&#039;https://gosales.quickbase.com/up/bsazkzsm2/a/r1/e22'/></tr></table>"
+//       },
+//       "33": {
+//         value: "67890"
+//       }
+//     }
+//   ],
+//   fields: [
+//     {
+//       id: 21,
+//       label: "Rep Card",
+//       type: "rich-text"
+//     },
+//     {
+//       id: 33,
+//       label: "Agent ID",
+//       type: "text"
+//     }
+//   ],
+//   metadata: {
+//     numFields: 2,
+//     numRecords: 1,
+//     skip: 0,
+//     totalRecords: 1
+//   }
+// };
 
-console.log(xbody.data[(0.21).value]);
+// console.log(xbody.data[(0.21).value]);
